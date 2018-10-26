9d1f730873a3b554e4d29163fce6ea791f849da5
JonathanRaines <jonathanraines@openbionics.com>
2018-10-26 16:32:43 +0100

    Started first draft and added libraries

---
+++ 02_FirstDraft/FirstDraft/FirstDraft/FirstDraft.ino
@@ -0,0 +1,19 @@
+/*
+ Name:		FirstDraft.ino
+ Created:	10/26/2018 4:15:40 PM
+ Author:	Jonathan
+*/
+
+#include "libraries\MPU9250\MPU9250.h"
+#include "libraries\quaternionFileters\quaternionFilters.h"
+#include <math.h>
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
