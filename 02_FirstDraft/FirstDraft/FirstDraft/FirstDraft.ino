18c90831f64f42ae35fb5dcb1385297ce2d616a6
JonathanRaines <jonathanraines@openbionics.com>
2018-10-26 16:33:29 +0100

    Added pin definitions

--- 02_FirstDraft/FirstDraft/FirstDraft/FirstDraft.ino
+++ 02_FirstDraft/FirstDraft/FirstDraft/FirstDraft.ino
@@ -8,6 +8,11 @@
 #include "libraries\quaternionFileters\quaternionFilters.h"
 #include <math.h>
 
+// Pin definitions
+int intPin = 2;  // These can be changed, 2 and 3 are the Arduinos ext int pins
+int myLed = 13;  // Set up pin 13 led for toggling
+int vibPin = 9;  // Vibration motor pin. 
+
 // the setup function runs once when you press reset or power the board
 void setup() {
 
